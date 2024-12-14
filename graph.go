4a3a3a1e0d4b6d5d69cf9a22b66f1a8b663898b0
Adrien Haxaire <adrien@haxaire.org>
2024-12-14 15:39:31 +0100

    Add Property constructors

--- graph.go
+++ graph.go
@@ -22,6 +22,22 @@ type Property struct {
 	Value propertyData
 }
 
+func StringProperty(key string, value string) Property {
+	return Property{key, propertyData{propertyDataType: text, stringData: value}}
+}
+
+func IntegerProperty(key string, value int64) Property {
+	return Property{key, propertyData{propertyDataType: integer, integerData: value}}
+}
+
+func DoubleProperty(key string, value float64) Property {
+	return Property{key, propertyData{propertyDataType: double, doubleData: value}}
+}
+
+func BooleanProperty(key string, value bool) Property {
+	return Property{key, propertyData{propertyDataType: boolean, booleanData: value}}
+}
+
 type Node struct {
 	Label      string
 	Properties []Property
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ func main() {
 	n1 := Node{"n1", nil}
 	n2 := Node{"n2", nil}
 
-	r12 := Relationship{n1, n2, nil}
+	p1 := StringProperty("p1-key", "p1-value")
+	p2 := IntegerProperty("p2-key", 2)
+	p3 := DoubleProperty("p3-key", 3.0)
+	p4 := BooleanProperty("p4-key", true)
+	r12 := Relationship{n1, n2, []Property{p1, p2, p3, p4}}
 
 	fmt.Println(r12)
 }
