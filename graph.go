4e51dd2101f933fbd0c7ed3af69c78c40e65865e
Adrien Haxaire <adrien@haxaire.org>
2024-12-14 12:38:03 +0100

    Add property data

--- graph.go
+++ graph.go
@@ -1,8 +1,25 @@
 package main
 
+type propertyType uint
+
+const (
+	text propertyType = iota
+	integer
+	double
+	boolean
+)
+
+type propertyData struct {
+	propertyDataType propertyType
+	stringData       string
+	integerData      int64
+	doubleData       float64
+	booleanData      bool
+}
+
 type Property struct {
 	Key   string
-	Value string
+	Value propertyData
 }
 
 type Node struct {
